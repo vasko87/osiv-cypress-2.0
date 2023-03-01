dcb07ab13fb7d0eba78e9ecdda0c4f76bc794aa3
vasko87 <masher.vasko@gmail.com>
2023-03-01 20:33:36 +0300

    generating the report

--- cypress/e2e/C44194_CorrectSupertextAndEntscheidtyp.js
+++ cypress/e2e/C44194_CorrectSupertextAndEntscheidtyp.js
@@ -2,19 +2,12 @@ import pages from "../support/base/OsivPageObject";
 import {c44194 as testData} from "../support/helpers/DataManager";
 
 describe(`C44194: (ENT ${testData.data1.entId}) Correct Supertext and Entscheidtyp`, () => {
-  testBody(testData.data1);
-});
+  [testData.data1, testData.data2].forEach((data) => {
+    before("Login", () => {
+      cy.loginWithSession(Cypress.env("username"), Cypress.env("password"));
+    });
 
-describe(`C44194: (ENT ${testData.data2.entId}) Correct Supertext and Entscheidtyp`, () => {
-  testBody(testData.data2);
-});
-
-function testBody(data) {
-  before("Login", () => {
-    cy.loginWithSession(Cypress.env("username"), Cypress.env("password"));
-  });
-
-  it(`Steps: Open ENT '${data.entId}', Click Korrekturenfunction button
+    it(`Steps: Open ENT '${data.entId}', Click Korrekturenfunction button
         select "Supertext, Entscheidtyp ändern" option from dynselect 
         >>> warning message '${data.warningMsg}' is presented
         confirm warning>>> info message '${data.infoMsg}' is presented
@@ -25,28 +18,29 @@ function testBody(data) {
         - 'Supertext, Entscheidtyp ändern' button is disabled;
         - Sendungs (VM, VB or MB) in status Neu are deleted;
         - Sendung (MIB) in status Korrigiert is not deleted`, () => {
-    pages.loginPage.openUrl();
-    pages.desktopMenu.navigateToEntscheidTab();
-    pages.entscheid.grid.searchAndOpenEntscheidID(data.entId);
-    pages.entscheid.detail
+      pages.loginPage.openUrl();
+      pages.desktopMenu.navigateToEntscheidTab();
+      pages.entscheid.grid.searchAndOpenEntscheidID(data.entId);
+      pages.entscheid.detail
         .checkSupertextDropdownReadOnly(true)
         .checkEntscheidTypDropdownReadOnly(true);
-    pages.entscheid.detail.ribbonMenu.clickKorrekturfunktionenBtn()
+      pages.entscheid.detail.ribbonMenu.clickKorrekturfunktionenBtn()
         .clickSubMenuMenuItem(pages.entscheid.detail.ribbonMenu.korrekturfunktionenSubMenu.supertextEntscheidtypandern);
-    pages.warningPopup.ckeckWarningContainsText(data.warningMsg)
+      pages.warningPopup.ckeckWarningContainsText(data.warningMsg)
         .clickOkBtn();
-    pages.infoPopup.ckeckInformationContainsText(data.infoMsg)
+      pages.infoPopup.ckeckInformationContainsText(data.infoMsg)
         .clickOkBtn();
-    pages.notification.checkSuccessMessageVisible();
+      pages.notification.checkSuccessMessageVisible();
 
-    pages.entscheid.detail.checkSupertextDropdownReadOnly(false);
-    pages.entscheid.detail.checkEntscheidTypDropdownReadOnly(false);
-    pages.entscheid.detail.ribbonMenu.clickKorrekturfunktionenBtn()
+      pages.entscheid.detail.checkSupertextDropdownReadOnly(false);
+      pages.entscheid.detail.checkEntscheidTypDropdownReadOnly(false);
+      pages.entscheid.detail.ribbonMenu.clickKorrekturfunktionenBtn()
         .checkSupertextEntscheidtypandernMenuItemEnable(false);
 
-    pages.entscheid.detail.tabBar.navigateToSendungenTab();
-    pages.entscheid.detail.sendungenGrid
+      pages.entscheid.detail.tabBar.navigateToSendungenTab();
+      pages.entscheid.detail.sendungenGrid
         .checkAllValuesInGridExist(data.sendungen.formilarNotVisible, false)
         .checkAllValuesInGridExist(data.sendungen.formilarVisible, true);
+    });
   });
-}
+});
