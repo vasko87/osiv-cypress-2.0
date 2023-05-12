e5cb09a8b48da193e3022ce1bcadac3296154fb1
maria <maria@maria.local>
2023-05-12 16:29:48 +0200

    pipeline config

--- cypress/support/page_objects/eingliederungen/details/popups/EingliederungsauftragErteilenPopup.js
+++ cypress/support/page_objects/eingliederungen/details/popups/EingliederungsauftragErteilenPopup.js
@@ -5,10 +5,14 @@ class EingliederungsauftragErteilenPopup extends EingliederungPageBase {
   constructor() {
     super(ModalWindowBase.css);
     this.modalWindow = new ModalWindowBase();
+    this.elements = {
+      createForm: () => this.elements.modalWindow().find("[akid='EingliederungsauftragCreateForm']")
+    }
   }
 
   waitForLoaded() {
     this.modalWindow.waitForLoaded();
+    this.elements.createForm().should("be.visible");
     return this;
   }
 }
