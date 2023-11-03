5c7288f1e817f9563b5781700ccc342d5cfb10d7
vasko87 <>
2023-11-03 16:00:45 +0100

    additional fix

--- cypress/e2e/OSIV3G_STA/C58211_AbgabeRegistrierenWithErrorMessages.js
+++ cypress/e2e/OSIV3G_STA/C58211_AbgabeRegistrierenWithErrorMessages.js
@@ -3,7 +3,6 @@ import flows from "../../support/base/OsivFlowsObject";
 import constants from "../../support/helpers/Constants";
 import {c58211 as testData} from "../../support/helpers/DataManager";
 import helperObject from "../../support/helpers/HelperObject";
-import pageBase from "../../support/base/PageBase";
 
 describe(`C58211: Abgabe registrieren _ with error messages; 
   TestRail:https://osiv.testrail.net/index.php?/cases/view/58211`, {failFast: {enabled: true}}, () => {
@@ -20,70 +19,70 @@ describe(`C58211: Abgabe registrieren _ with error messages;
   it(`Step 2: Open Dossier Chronik>Dossier Abgabe tabP; Click Abgabe kontrollieren button =>
       there are 2 failures are presented;`, () => {
     pages.versicherte.detail.sideMenu.navigateToDossierChronikTab();
-    //      .clickDossierAbgabeTab();
-    // pages.versicherte.detail.ribbonMenu.clickAgabeKontrollierenBtn();
-    // pages.waitForLoadingDisappears();
+         .clickDossierAbgabeTab();
+    pages.versicherte.detail.ribbonMenu.clickAgabeKontrollierenBtn();
+    pages.waitForLoadingDisappears();
   });
 
   it(`Step 3: Click on the first error with sendung >sendung details page opens
       Click Abschlissen for sendung
       Click OK on abschlissen popup >senung is closed`, () => {
-    // pages.versicherte.detail.dossierChronikTab.dossierAbgabeGrid.waitGridViewLoaded()
-    //      .dblClickRowWithText("Die Sendung");
-    // pages.sendungen.detail.waitForLoaded();
-    // pages.sendungen.detail.ribbonMenu.clickAbschliessenBtn();
-    // pages.sendungen.detail.sendungenAbschliessenPopup.modalWindow.waitForLoaded();
-    // cy.wait(2000);
-    // pages.sendungen.detail.sendungenAbschliessenPopup.modalWindow.clickOkBtn();
-    // cy.wait(2000);
-    // pages.notification.waitForSuccessMessageDisappears();
+    pages.versicherte.detail.dossierChronikTab.dossierAbgabeGrid.waitGridViewLoaded()
+         .dblClickRowWithText("Die Sendung");
+    pages.sendungen.detail.waitForLoaded();
+    pages.sendungen.detail.ribbonMenu.clickAbschliessenBtn();
+    pages.sendungen.detail.sendungenAbschliessenPopup.modalWindow.waitForLoaded();
+    cy.wait(2000);
+    pages.sendungen.detail.sendungenAbschliessenPopup.modalWindow.clickOkBtn();
+    cy.wait(2000);
+    pages.notification.waitForSuccessMessageDisappears();
   });
 
   it(`Step 4: Go back to dossier-abgabe`, () => {
-    // pages.groupedTaskbar.clickContainsVersichertendatenTab();
+    pages.groupedTaskbar.clickContainsVersichertendatenTab();
   });
 
   it(`Step 5: Click on error related to ENT>ent details opens
       Click Berabeiten einleiten`, () => {
-    // pages.versicherte.detail.dossierChronikTab.dossierAbgabeGrid.waitGridViewLoaded()
-    //      .dblClickRowWithText("Der Entscheid");
-    // pages.entscheid.detail.waitForLoaded();
-    // pages.entscheid.detail.ribbonMenu.clickBearbeitungEinleitenBtn();
-    // pages.waitForLoadingDisappears();
-    // pages.modalWindow.clickOkBtn();
-    // pages.notification.checkSuccessMessageVisibleAndWaitForDisappeared();
+    pages.versicherte.detail.dossierChronikTab.dossierAbgabeGrid.waitGridViewLoaded()
+         .dblClickRowWithText("Der Entscheid");
+    pages.entscheid.detail.waitForLoaded();
+    pages.entscheid.detail.ribbonMenu.clickBearbeitungEinleitenBtn();
+    pages.waitForLoadingDisappears();
+    pages.modalWindow.clickOkBtn();
+    pages.notification.checkSuccessMessageVisibleAndWaitForDisappeared();
   });
 
   it(`Step 6: Go back to dossier-abgabe`, () => {
-    // pages.groupedTaskbar.clickContainsVersichertendatenTab();
-    // pages.waitForLoadingDisappears();
+    pages.groupedTaskbar.clickContainsVersichertendatenTab();
+    pages.waitForLoadingDisappears();
   });
 
   it(`Step7: Click Abgabe registrieren button
       Select Dossier Abgabe an
       Click OK
       confirm warning`, () => {
-    // pages.versicherte.detail.ribbonMenu.clickAbgabeRegistrierenBtn()
-    //      .selectDosseirAbgabeAnDropdown("301")
-    //      .clickOkBtn();
-    // pages.warningPopup.clickOkBtn();
+    pages.versicherte.detail.ribbonMenu.clickAbgabeRegistrierenBtn()
+         .selectDosseirAbgabeAnDropdown("301")
+         .clickOkBtn();
+    pages.warningPopup.clickOkBtn();
   });
 
   it(`Expected: info panel message appears - (OSCSTAMM:152)
       button abgabe durchfuhren is enabled
       no errors/warnings in the list
       VP status = abgegeben`, () => {
-    // pages.checkMsgWarningContainsText(constants.MSG.OSCSTAMM_152);
-    // pages.versicherte.detail.ribbonMenu.checkAbgabeDurchfuhrenBtnDisabled(false);
-    // pages.versicherte.detail.dossierChronikTab.dossierAbgabeGrid.checkGridRowsCount(0);
-    // pages.versicherte.detail.sideMenu.navigateToBasisdatenTab().checkStatusTxt(testData.vpStatus);
+    pages.checkMsgWarningContainsText(constants.MSG.OSCSTAMM_152);
+    pages.versicherte.detail.ribbonMenu.checkAbgabeDurchfuhrenBtnDisabled(false);
+    pages.versicherte.detail.dossierChronikTab.dossierAbgabeGrid.checkGridRowsCount(0);
+    pages.versicherte.detail.sideMenu.navigateToBasisdatenTab().checkStatusTxt(testData.vpStatus);
   });
 
   it(`Expected: additional check: ENTs are closed and abgegeben`, () => {
-    // pages.versicherte.detail.tabBar.navigateToEntscheideTab()
-    //      .grid.waitGridViewLoaded()
-    //      .checkTwoTextsExistInRow("Abgegeben", "Abgeschlossen", 1)
-    //      .checkTwoTextsExistInRow("Abgegeben", "Abgeschlossen", 2);
+    pages.versicherte.detail.tabBar.navigateToEntscheideTab()
+         .grid.waitGridViewLoaded()
+         .checkTwoTextsExistInRow("Abgegeben", "Abgeschlossen", 1)
+         .checkTwoTextsExistInRow("Abgegeben", "Abgeschlossen", 2);
   });
 
   it(`Expected: gesuch is closed`, () => {
