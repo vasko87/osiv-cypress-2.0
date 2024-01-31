e93f90d8676be03c804213da67d4024058078a9b
vasko87 <>
2024-01-31 15:24:21 +0100

    fix for text Editor highlighted part

--- cypress/support/standalone/TxtEditor.js
+++ cypress/support/standalone/TxtEditor.js
@@ -90,7 +90,7 @@ class TxtEditor {
 
   checkTextHighlighted(text, isHighlighted) {
     pageBase.executeJS(`document.querySelector("${this.baseCSS}").vueComponentInstance.getValue()`).then((val) => {
-      const highlightedTextExists = val.indexOf(`chcbpat3 ${text}`) !== -1;
+      const highlightedTextExists = val.indexOf(`chcbpat6 ${text}`) !== -1;
       if (isHighlighted) {
         expect(highlightedTextExists).to.be.eql(true);
       } else {
